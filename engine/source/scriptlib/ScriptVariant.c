0192a1007533fc0b61563998df8e7db47cd6de82
Malik <31506138+msmalik681@users.noreply.github.com>
2021-02-21 11:26:53 +0000

    Velocity bug fix.
    This will fix the velocity bug when script moves player to the left they fly off to the right.

--- engine/source/scriptlib/ScriptVariant.c
+++ engine/source/scriptlib/ScriptVariant.c
@@ -917,8 +917,10 @@ void ScriptVariant_Neg( ScriptVariant *svar)
     {
     case VT_DECIMAL:
         svar->dblVal = -(svar->dblVal);
+        break;
     case VT_INTEGER:
         svar->lVal = -(svar->lVal);
+        break;
     default:
         break;
     }
